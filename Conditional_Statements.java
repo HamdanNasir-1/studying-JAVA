81718bdd5b933169c7e5f5d0b14564898b35a5b7
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-04 16:04:55 +0530

    practicing

--- Conditional_Statements.java
+++ Conditional_Statements.java
@@ -1,5 +1,11 @@
 public class Conditional_Statements {
     public static void main(String[] args) {
-        
+        int a = 5;
+        if(a==5){
+            
+            System.out.println("The value of a is 5");
+        }
+
+        System.out.println("The cosde executed correctly");
     }
 }
