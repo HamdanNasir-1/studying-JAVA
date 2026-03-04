09f1da65a5d2165adfba24084d662a18fd315ad7
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-04 16:08:45 +0530

    practicing

--- Conditional_Statements.java
+++ Conditional_Statements.java
@@ -1,14 +1,19 @@
 public class Conditional_Statements {
     public static void main(String[] args) {
         int a = 7;
-        if(a==5){
+        // if(a==5){
             
-            System.out.println("The value of a is 5");
-        }
-        else{
-            System.out.println("The value of a is not 5 and this code lying");
-        }
+        //     System.out.println("The value of a is 5");
+        // }
+        // else{
+        //     System.out.println("The value of a is not 5 and this code lying");
+        // }
 
-        System.out.println("The code executed sucessfully");
+        // System.out.println("The code executed sucessfully");
+
+        if(a%2==0){
+            System.out.println("The value is even");
+        }
+        System.out.println("The value is oddd");
     }
 }
