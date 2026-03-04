ec67f2c8dbc45c61ab1ab7315d5f29e4fa367c4d
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-04 16:24:27 +0530

    ptracticing

--- Conditional_Statements.java
+++ Conditional_Statements.java
@@ -17,18 +17,35 @@ public class Conditional_Statements {
         // else{
         //     System.out.println("The value is oddd");
         // }
-        int i = 5;
 
-        if (i == 5) {
-            System.out.println("The value of i 5");
-        } else if (i == 6) {
-            System.out.println("The value of i 6");
-        } else if (i == 7) {
-            System.out.println("The value of i 7");
-        } else if (i == 8) {
-            System.out.println("The value of i 8");
-        } else if (i == 9) {
-            System.out.println("The value of i 9");
+        // if else ladder
+        // int i = 5;
+        // if (i == 5) {
+        //     System.out.println("The value of i 5");
+        // } else if (i == 6) {
+        //     System.out.println("The value of i 6");
+        // } else if (i == 7) {
+        //     System.out.println("The value of i 7");
+        // } else if (i == 8) {
+        //     System.out.println("The value of i 8");
+        // } else if (i == 9) {
+        //     System.out.println("The value of i 9");
+        // }
+
+        // A little program
+
+        int i = 50;
+
+        if (i == 50) {
+            System.out.println("You are and old person");
+        } else if (i == 40) {
+            System.out.println("You are becoming old");
+        } else if (i == 30) {
+            System.out.println("You are a man now");
+        } else if (i == 20) {
+            System.out.println("Ypu just crossed the teen age  ");
+        } else {
+            System.out.println("Jaake mummy ka doodh pio beta");
         }
     }
 }
