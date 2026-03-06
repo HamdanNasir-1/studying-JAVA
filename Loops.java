0ceea3086fb8ebc1fb4f162c766024917939c51b
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-06 08:51:20 +0530

    practice

--- Loops.java
+++ Loops.java
@@ -1,15 +1,24 @@
+
 public class Loops {
+
     public static void main(String[] args) {
-        int i = 5;
+        // int i = 5;
 
         // while(i<=10){
         //     System.out.println(i);
         //     i++;
         // }
-        do{
-            System.out.println("Hello hamdan bhai ");
-            i++;
-        }while(i<5);
+        // do{
+        //     System.out.println("Hello hamdan bhai ");
+        //     i++;
+        // }while(i<5);
+        for (int i = 10; i >= 1; i--) {
+            for (int j = 10; j >= i; j--) {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
     }
-    
+
 }
