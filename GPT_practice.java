594fae29f3b0e9653cfcfffa0f700f11dfcaefa4
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-06 07:37:15 +0530

    practicing

---
+++ GPT_practice.java
@@ -0,0 +1,14 @@
+public class GPT_practice {
+    public static void main(String[] args) {
+        
+        int num = -1;
+
+        if(num == 0){
+            System.out.println("The number zero");
+        }
+        else if(num<0){
+            System.out.println("Negative number");
+        }
+        else{}
+    }
+}
