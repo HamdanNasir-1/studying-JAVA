32ed643708e96b6f79b9ce553e10bd8a4fbd8f71
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-06 07:42:55 +0530

    [ractice

--- GPT_practice.java
+++ GPT_practice.java
@@ -1,14 +1,18 @@
 public class GPT_practice {
     public static void main(String[] args) {
         
-        int num = 1;
+        int num1 = 1;
+        int num2 = 2;
 
-        if(num%2==0){
-            System.out.println("The number is even");
+        if(num1 < num2){
+            System.out.println("The num2 is greater");
         }
         else{
-            System.out.println("The number is Odd");
+            System.out.println("The num1 is greater");
+
         }
+
+        
         
     }
 }
