a16acad5cfd0abec1d4eeca2707bfdc87c393dcd
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 16:07:58 +0530

    praciting

--- Arrays.java
+++ Arrays.java
@@ -12,17 +12,16 @@ public class Arrays {
         // // arr[] = 15;
         // System.out.println("Here is the first array program "+arr[0]);
         // --:Array using loops :--
-    //     for (int i = 0; i < arr.length; i++) {
-    //         arr[i] = x;
-    //         x++;
-    //     }
-    //     for (int i = 0; i < arr.length; i++) {
-    //         System.out.println("The value for index "+i+" is "+arr[i]);
-    //     }
-
+        //     for (int i = 0; i < arr.length; i++) {
+        //         arr[i] = x;
+        //         x++;
+        //     }
+        //     for (int i = 0; i < arr.length; i++) {
+        //         System.out.println("The value for index "+i+" is "+arr[i]);
+        //     }
         int[][] arr = new int[3][3];
-        arr[0][0] = 50; 
-        arr[0][1] = 30; 
+        arr[0][0] = 50;
+        arr[0][1] = 30;
         arr[0][2] = 90;
         arr[1][0] = 60;
         arr[1][1] = 40;
@@ -31,6 +30,8 @@ public class Arrays {
         arr[2][1] = 60;
         arr[2][2] = 70;
 
-        
+        for (int row = 0; row < 3; row++) {
+            
+        }
     }
 }
