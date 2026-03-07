0fc1ec4d7d35c093bf4ba11d44b3b8940e30b1e9
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 21:10:57 +0530

    praciticing

--- Arrays.java
+++ Arrays.java
@@ -45,6 +45,11 @@ public class Arrays {
         arr[2][1] = 16;
         arr[2][2] = 17;
 
-        
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
