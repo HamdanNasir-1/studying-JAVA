d7ce84a5e180c5f46d76d4eb21291ceddce6782a
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 22:49:09 +0530

    practicing

--- Methods.java
+++ Methods.java
@@ -1,31 +1,56 @@
+
 public class Methods {
+
     public static void main(String[] args) {
 
-        NoINPOP ();
-        NamsSayer("Hamdan");
-        System.out.println(Output());
-        System.out.println(Multiplier(2,4));
+        //     NoINPOP ();
+        //     NamsSayer("Hamdan");
+        //     System.out.println(Output());
+        //     System.out.println(Multiplier(2,4));
+        // Method ovweloading
+        System.out.println(Sum(2, 3));
+        System.out.println(Sum(2, 3, 6));
+        System.out.println(Sum(3.141592653589793, 3));
+        greet(45, "Hamdan");
+        greet("Hamdan",45);
 
-        
     }
 
-    // NO input and output
-    static void NoINPOP (){
-        System.out.println("Hello Hamdan Bhai");
+    // // NO input and output
+    // static void NoINPOP (){
+    //     System.out.println("Hello Hamdan Bhai");
+    // }
+    // // Input but no Output
+    // static void NamsSayer(String name){
+    //     System.out.println("Hello "+name);
+    // }
+    // // No input but Output
+    // static int Output(){
+    //     return 10;
+    // }
+    // // Output and Input
+    // static int Multiplier(int a, int b){
+    //     return (a*b);
+    // }
+
+    // Method ovweloading
+    static int Sum(int a, int b) {
+        return a * b;
     }
 
-    // Input but no Output
-    static void NamsSayer(String name){
-        System.out.println("Hello "+name);
+    static int Sum(int a, int b, int c) {
+        return a + b + c;
     }
 
-    // No input but Output
-    static int Output(){
-        return 10;
+    static int Sum(double d, double f) {
+        return (int) (d * f);
     }
 
-    // Output and Input
-    static int Multiplier(int a, int b){
-        return (a*b);
+    static void greet(String name, int age) {
+        System.out.println("Hello " + name + " of age " + age);
+    }
+
+    static void greet(int age, String name) {
+        System.out.println("Hello " + name + " of age " + age);
     }
 }
