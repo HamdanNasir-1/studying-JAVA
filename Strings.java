dca26cae04b9224362c78bc0cabb0e073765c6c2
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 21:25:59 +0530

    practicing

--- Strings.java
+++ Strings.java
@@ -1,8 +1,10 @@
+
 public class Strings {
+
     public static void main(String[] args) {
         String Fname = "Md Hamdan";
         String Lname = "Nasir";
 
-        System.out.println(Fname+Lname);
+        System.out.println(Fname + " " + Lname);
     }
 }
