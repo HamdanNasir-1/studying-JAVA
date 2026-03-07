8b8b35bd33fa82fa657e9f899b1fc410b030e584
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-08 01:42:46 +0530

    practicing

--- Method_Chaining.java
+++ Method_Chaining.java
@@ -1,5 +1,10 @@
 public class Method_Chaining {
     public static void main(String[] args) {
-        
+        name("Hamdan");;
+    }
+
+    
+    static void name(String name){
+        System.out.println("Hello "+name);
     }
 }
