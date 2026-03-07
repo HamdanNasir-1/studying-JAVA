03f6ffc8c99a4c58b0fdeb86c816de7e75269714
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 15:33:05 +0530

    practicing

--- Arrays.java
+++ Arrays.java
@@ -1,14 +1,19 @@
+
 public class Arrays {
+
     public static void main(String[] args) {
-        
+
         int arr[] = new int[4];
 
-        arr[0] = 12;
-        arr[1] = 13;
-        arr[2] = 14;
-        arr[3] = 15;
-        // arr[] = 15;
-
-        System.out.println("Here is the first array program "+arr[0]);
+        // arr[0] = 12;
+        // arr[1] = 13;
+        // arr[2] = 14;
+        // arr[3] = 15;
+        // // arr[] = 15;
+        // System.out.println("Here is the first array program "+arr[0]);
+        // Array using loops :--
+        for (int i = 0; i <= arr.length; i++) {
+            arr
+        }
     }
 }
