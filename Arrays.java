9924061f3ffab8c3e2d07a3ed5ac7f621eea61ec
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 15:31:14 +0530

    [ractiting

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,14 @@
 public class Arrays {
     public static void main(String[] args) {
         
+        int arr[] = new int[4];
+
+        arr[0] = 12;
+        arr[1] = 13;
+        arr[2] = 14;
+        arr[3] = 15;
+        // arr[] = 15;
+
+        System.out.println("Here is the first array program "+arr[0]);
     }
 }
