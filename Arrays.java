31ccfba76f55397f44dd07d1ad7c3030d93254f4
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 15:56:57 +0530

    practicing

--- Arrays.java
+++ Arrays.java
@@ -3,8 +3,8 @@ public class Arrays {
 
     public static void main(String[] args) {
 
-        int arr[] = new int[4];
-        int x = 101;
+        // int arr[] = new int[4];
+        // int x = 101;
         // arr[0] = 12;
         // arr[1] = 13;
         // arr[2] = 14;
@@ -19,5 +19,7 @@ public class Arrays {
     //     for (int i = 0; i < arr.length; i++) {
     //         System.out.println("The value for index "+i+" is "+arr[i]);
     //     }
+
+
     }
 }
