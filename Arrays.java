e72fb941e4f1ebdc84caf6caf7b5553c25d2e609
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-07 16:04:33 +0530

    practicing

--- Arrays.java
+++ Arrays.java
@@ -20,6 +20,16 @@ public class Arrays {
     //         System.out.println("The value for index "+i+" is "+arr[i]);
     //     }
 
+        int[][] arr = new int[3][3];
+        arr[0][0] = 50; 
+        arr[0][1] = 30; 
+        arr[0][2] = 90;
+        arr[1][0] = 60;
+        arr[1][1] = 40;
+        arr[1][2] = 60;
+        arr[2][0] = 70;
+        arr[2][1] = 60;
+        arr[2][2] = 70;
 
     }
 }
