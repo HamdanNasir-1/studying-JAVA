48d4d069c46037af5ee3163e3bdacac265012611
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-09 02:14:36 +0530

    practicing

--- Constructor.java
+++ Constructor.java
@@ -6,10 +6,10 @@ public class Constructor {
         // s1.age= 34;
         // s1.rollNumber = 5004;
         // s1.name = "Integral University";
-        System.out.println(s1.name);
-        System.out.println(s1.age);
-        System.out.println(s1.rollNumber);
-        System.out.println(s1.college);
+        System.out.println("The name is "+s1.name);
+        System.out.println("The age is "+s1.age);
+        System.out.println("The rollnumber is "+s1.rollNumber);
+        System.out.println("The college name is "+s1.college);
     }
 }
 // Default constructor or normal constructor 
