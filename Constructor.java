62d95137374d6139c1e1ef74f4aa5105a669f710
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-10 00:54:52 +0530

    practicing

--- Constructor.java
+++ Constructor.java
@@ -3,6 +3,9 @@ public class Constructor {
 
     public static void main(String[] args) {
         Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Student s4 = new Student();
 
         // s1.name = "Hamdan";
         // s1.age= 34;
