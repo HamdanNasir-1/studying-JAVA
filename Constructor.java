0aa852f511512192d19d310b7b4f5afca1122bc1
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-10 00:56:58 +0530

    practicing

--- Constructor.java
+++ Constructor.java
@@ -3,18 +3,15 @@ public class Constructor {
 
     public static void main(String[] args) {
         Student s1 = new Student();
-        Student s2 = new Student();
-        Student s3 = new Student();
-        Student s4 = new Student();
+        Student s2 = new Student("Hamdan");
+        Student s3 = new Student("Hamdan",23);
+        Student s4 = new Student("Hamdan",34,34,"Integral University");
 
         // s1.name = "Hamdan";
         // s1.age= 34;
         // s1.rollNumber = 5004;
         // s1.name = "Integral University";
-        System.out.println("The name is " + s1.name);
-        System.out.println("The age is " + s1.age);
-        System.out.println("The rollnumber is " + s1.rollNumber);
-        System.out.println("The college name is " + s1.college);
+        
     }
 }
 // Default constructor or normal constructor 
