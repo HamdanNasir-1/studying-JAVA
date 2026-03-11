e18dd9f5e04d048ac600c4cd29c4e3a478736f48
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-11 14:34:19 +0530

    practicing

--- Encapsulation.java
+++ Encapsulation.java
@@ -2,6 +2,23 @@
 public class Encapsulation {
 
     public static void main(String[] args) {
-        
+
+    }
+}
+
+class Bank {
+
+    private double balance = 0;
+
+    public void submitMoney(int Amount) {
+        balance += Amount;
+    }
+
+    public void withdrawMoney(int Amount) {
+        balance -= Amount;
+    }
+
+    public double getBalance() {
+        return balance;
     }
 }
