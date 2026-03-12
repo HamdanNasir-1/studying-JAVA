2e072480025c663f75c6f779c8dfdcfbdb9d104f
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-13 01:13:33 +0530

    practicing

--- Inheritance.java
+++ Inheritance.java
@@ -1,17 +1,19 @@
 public class Inheritance {
     public static void main(String[] args) {
-
+        Vehicle s1 = new Vehicle();
+        s1.start();
+        
     }
 }
 
 class Vehicle {
     String name;
-
-    void start() {
-        System.out.println("GGGHHHrrrrrrrrrr");
-    }
+    
 }
 
 class Car extends Vehicle {
-    
+    void start() {
+        System.out.println("GGGHHHrrrrrrrrrr");
+    }
+
 }
