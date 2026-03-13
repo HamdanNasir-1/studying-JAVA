69e9df53f456b10640159445963ccd9e3408df29
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-14 01:38:39 +0530

    practicing

--- Interface.java
+++ Interface.java
@@ -1,10 +1,13 @@
 public class Interface {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
-interface Car{
+
+interface Car {
     void Start();
+
     void accelerattion();
+
     void brake();
 }
