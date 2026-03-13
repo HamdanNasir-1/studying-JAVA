edfdc5e222d97f42876811e132f0eee7bd94eaa9
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-14 00:53:50 +0530

    practicing

--- AbstractClass.java
+++ AbstractClass.java
@@ -1,7 +1,9 @@
 public class AbstractClass {
     public static void main(String[] args) {
         Car fCar = new fuelCar();
+        Car eCar = new eCar();
         fCar.start();
+        eCar.start();
     }
 }
 
@@ -17,6 +19,7 @@ abstract class Car {
 class fuelCar extends Car {
 
     void start() {
+
         System.out.println("The fuel car started");
     }
 
@@ -28,4 +31,23 @@ class fuelCar extends Car {
         System.out.println("The fuel car is stopping");
 
     }
+}
+
+class eCar extends Car {
+
+    void start() {
+        System.out.println("The Electric car car is starting");
+
+    }
+
+    void accelerate() {
+        System.out.println("The Electric car is accelearted at 50km/h");
+
+    }
+
+    void brake() {
+        System.out.println("The Electric car car is stopping");
+
+    }
+
 }
