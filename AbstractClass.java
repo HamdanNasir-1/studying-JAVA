823e3960e67d6bdff6e4b206378e7c0a8f85618c
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-14 00:31:20 +0530

    practicing

--- AbstractClass.java
+++ AbstractClass.java
@@ -1,10 +1,12 @@
 public class AbstractClass {
     public static void main(String[] args) {
-        
+
     }
 }
-class Car{
-    void start(){
+
+class Car {
+    void start() {
         System.out.println("The car is starting");
     }
+
 }
