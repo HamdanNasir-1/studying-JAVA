0af9dada4ecf04aeb71fc9ef71a731048543a03c
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-03-17 01:29:50 +0530

    practicing

--- A_boxing_Unboxing.java
+++ A_boxing_Unboxing.java
@@ -1,12 +1,13 @@
 public class A_boxing_Unboxing {
     public static void main(String[] args) {
         // Autoboxing :---
-        int a = 10;
-        Integer b = a;
+        // int a = 10;
+        // Integer b = a;
 
-        System.out.println(a);
-        System.out.println(b);  //Unboxing  
+        // System.out.println(a);
+        // System.out.println(b);  //Unboxing  
 
-        
+        Integer s = 10;
+        int d = 10;
     }
 }
