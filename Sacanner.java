acb63087a988b88387639d3b08210d74c89f639a
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-21 14:32:43 +0530

    practicing

---
+++ Sacanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Sacanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println("This is your age : "+age);
+
+        // System.out.println("This is your name : "+name);
+    }
+}
---
+++ Scanner_Practice.java
@@ -0,0 +1,5 @@
+public class Scanner_Practice{
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
+}
