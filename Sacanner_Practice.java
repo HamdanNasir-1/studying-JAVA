37d35a8261e816ec716a387f360e2f3dc8e841b6
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-21 15:09:55 +0530

    practicing

--- Sacanner_Practice.java
+++ Sacanner_Practice.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 public class Sacanner_Practice{
     public static void main(String args[]){
-        System.out.println("hello");
+        // System.out.println("hello");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your name");
-        String name = sc.nextLine();
+        // System.out.println("Please enter your name");
+        // String name = sc.nextLine();
 
-        System.out.println(name  + " is you name" );
+        // System.out.println(name  + " is you name" );
+
+        System.out.println("Please any two number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = a + b;
+        
+        System.out.println("This is the addition : "+c);
     }
 }
