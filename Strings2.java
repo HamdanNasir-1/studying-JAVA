e4938590332547832c56102dc5185a619d7fabe6
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-23 22:21:20 +0530

    practicing

---
+++ Strings2.java
@@ -0,0 +1,22 @@
+
+public class Strings2 {
+    public static void main(String[] args) {
+        String s1 = new String();
+        
+        String s2 = new String("Hamdan");
+        // System.out.println(s1);
+        // System.out.println(s2);
+
+        String s3 = "hamdan";//String pool
+        String s4 = new String(s3);//Heap memory      
+
+        // System.out.println(s3);
+
+        char arr[] = {'H','a','m','d','a','n'};
+        String s5 = new String(arr);
+
+        System.out.println(s5);
+
+
+    }
+}
