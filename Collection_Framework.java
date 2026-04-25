3c778b4bf0075916c84d4ff607b2a598044cb5a0
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-25 18:07:49 +0530

    practicing

--- Collection_Framework.java
+++ Collection_Framework.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 
 public class Collection_Framework {
     public static void main(String[] args) {
-        ArrayList<String> it = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Hamdan");
+        list.add("Arsh");
+        list.add("Arsalan");
+        list.add("Shoaib");
+        list.add("Rehan");
     }
 }
