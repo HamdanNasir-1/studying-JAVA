2ee081bca4a8ec7ed98d7dfa4a731ca614f25d5d
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-25 18:17:43 +0530

    practicing

--- Linked_list.java
+++ Linked_list.java
@@ -3,6 +3,12 @@ import java.util.LinkedList;
 
 public class Linked_list {
     public static void main(String[] args) {
+
         LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(67);
+        list.add(87);
+        list.add(98);
+        list.add(90);
     }
 }
