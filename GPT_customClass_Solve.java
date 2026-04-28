0aa5cf09470107da938c89df4f6893e102d380b9
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-28 17:34:12 +0530

    practicing

--- GPT_customClass_Solve.java
+++ GPT_customClass_Solve.java
@@ -1,16 +1,28 @@
+import java.util.*;
+
 public class GPT_customClass_Solve {
     public static void main(String[] args) {
 
-    }
+        List<Student> details = new ArrayList<>();
+        details.add(new Student("Hamdan", 22));
+        details.add(new Student("Zulfakkar", 25));
+        details.add(new Student("Salimun", 67));
 
-    class Student {
-        String name;
-        int age;
-
-        public Student(String name, int age) {
-            this.name = name;
-            this.age = age;
+        for(Student s:details){
+            System.out.println("Name: "+s.name+" Age:"+s.age);
         }
-
+        
     }
+}   
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+
 }
