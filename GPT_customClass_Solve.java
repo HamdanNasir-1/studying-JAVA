57468901fe5c27f454c1df58a608ff50a526f2e3
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-04-28 17:15:29 +0530

    practicin

--- GPT_customClass_Solve.java
+++ GPT_customClass_Solve.java
@@ -1,5 +1,16 @@
 public class GPT_customClass_Solve {
     public static void main(String[] args) {
-        
+
+    }
+
+    class Student {
+        String name;
+        int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
     }
 }
